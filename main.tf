ff451b271fc6d753c9be83b5ac7f4f4a866a653f
charanvarma <charankumarvarma@gmail.com>
2021-10-22 21:32:26 -0400

    Proper Resources

--- main.tf
+++ main.tf
@@ -118,15 +118,4 @@ resource "aws_instance" "myapp_server" {
 
     associate_public_ip_address=true
     key_name=aws_key_pair.ssh-key.key_name
-    user_data = <<EOF
-                    #!/bin/bash
-                    sudo yum update -y && sudo yum install -y docker
-                    sudo systemctl start docker
-                    sudo usermod -aG docker ec2-user
-                    docker run -p 8080:80 nginx 
-                E0F
-    tags=
-        {
-        Name= ${var.env_prefix}-server
-        }
-    }
+}
